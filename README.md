5d97e8e4850f35feef452499a79e407e2c58c8c5
Andre Sionek <andresionek91@gmail.com>
2023-12-30 13:07:40 +0000

    feat: implement cdk package

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# book-package
+# Org CDK
+
+A package that extends [AWS CDK](https://aws.amazon.com/cdk/) constructs and implement some common patterns.
+
+## Instalation
+
+With pip:
+
+```bash
+pip install git+https://github.com/andresionek91/book-package.git@<release or tag>
+```
+
+With poetry:
+
+```bash
+poetry add  git+https://github.com/andresionek91/book-package.git@<release or tag>
+```
+
+## Usage
+
+This package implements some L2 and L3 cdk constructs that can be imported and used across different projects.
---
+++ tests/local_app.py
@@ -0,0 +1,36 @@
+"""
+Use this class for local cdk synth, manual deploy to development account, and to run checkov tests
+"""
+
+import aws_cdk as cdk
+
+from constructs import Construct
+
+from org_cdk.org_s3 import OrgBucket
+
+
+class LocalTestBucketStack(cdk.Stack):
+    """Local test/deploy of bucket"""
+
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+        bucket = OrgBucket(
+            scope=self,
+            id="Bucket",
+            name="test-bucket",
+        )
+
+        bucket.apply_removal_policy(policy=cdk.RemovalPolicy.DESTROY)
+
+
+env = cdk.Environment(account="123456789012", region="us-east-1")
+
+app = cdk.App()
+
+LocalTestBucketStack(
+    scope=app,
+    id="LocalTestBucketStack",
+    env=env,
+)
+
+app.synth()
