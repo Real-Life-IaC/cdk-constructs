b6b95423f4e89a6b4d0821124bef0288c7c2e21f
Andre Sionek <andresionek91@gmail.com>
2024-01-09 17:51:21 +0000

    feat: create enums for region and stage

--- constructs_package/constants.py
+++ constructs_package/constants.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 
-class AwsAccountId(Enum, str):
+class AwsAccountId(str, Enum):
     """AWS account IDs"""
 
     SANDBOX = "637423243766"
@@ -9,3 +9,19 @@ class AwsAccountId(Enum, str):
     PRODUCTION = "730335548799"
     SHARED_SERVICES = "767397808306"
     MANAGEMENT = "267631547124"
+
+
+class AwsRegion(str, Enum):
+    """AWS regions"""
+
+    US_EAST_1 = "us-east-1"
+
+
+class AwsStage(str, Enum):
+    """AWS stages"""
+
+    SANDBOX = "sandbox"
+    STAGING = "staging"
+    PRODUCTION = "production"
+    SHARED_SERVICES = "shared_services"
+    MANAGEMENT = "management"
